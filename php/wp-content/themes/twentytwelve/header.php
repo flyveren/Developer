7da07f017cd16cacb4762bf62a2252dc47cbc804
flyveren <flyveren@gmail.com>
2013-07-17 18:20:29 +0200

    testing theme

--- php/wp-content/themes/twentytwelve/header.php
+++ php/wp-content/themes/twentytwelve/header.php
@@ -37,6 +37,7 @@
 		<hgroup>
 			<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
 			<h2 class="site-description"><?php bloginfo( 'description' ); ?></h2>
+            <h2>testing themes</h2>
 		</hgroup>
 
 		<nav id="site-navigation" class="main-navigation" role="navigation">
